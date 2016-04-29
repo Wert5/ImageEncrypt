6a02b7d0fdbad5a4689ad4c7a85b5c3fd766c3e6
sonyal <rabbitzrokc3@gmail.com>
2016-04-29 13:30:48 -0400

    Added files via upload

---
+++ ImageEncryptGUI.java
@@ -0,0 +1,43 @@
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Scanner;
+
+import javax.swing.*;
+import javax.swing.filechooser.FileNameExtensionFilter;
+
+import java.awt.*;
+
+/** JDK 7+. */
+public class ImageEncryptGUI extends JFrame {
+	JButton save = new JButton("Save");
+	JButton encrypt = new JButton("Encrypt");
+	JButton browse = new JButton("Browse");
+	JButton decrypt = new JButton("Decrypt");
+	JTextField password = new JTextField("password");
+	JTextField filePath = new JTextField("filePath");
+	JButton OK = new JButton("OK");
+	JPanel South = new JPanel();
+  
+  public static void main(String... aArgs) {
+	  ImageEncryptGUI sonya = new ImageEncryptGUI();
+	  sonya.setVisible(true);
+	  
+  }
+  
+  public ImageEncryptGUI(){
+	  this.setSize(400,500);
+	  this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	  this.setLayout(new BorderLayout());
+	  this.setVisible(true);
+	  
+	  this.add(save,BorderLayout.NORTH);
+	  this.add(South,BorderLayout.SOUTH);
+	  South.setLayout(new GridLayout(4,2));
+	  South.add(encrypt);
+	  South.add(decrypt);
+	  
+  }
+  
+}
