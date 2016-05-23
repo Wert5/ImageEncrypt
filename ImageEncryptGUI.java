435b1bf3ae5a32fda127eae4b11625cec92a6f88
sonyal <rabbitzrokc3@gmail.com>
2016-05-23 12:58:34 -0400

    Update ImageEncryptGUI.java

--- ImageEncryptGUI.java
+++ ImageEncryptGUI.java
@@ -8,6 +8,8 @@ import javax.swing.*;
 import javax.swing.filechooser.FileNameExtensionFilter;
 
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 /** JDK 7+. */
 public class ImageEncryptGUI extends JFrame {
@@ -19,25 +21,34 @@ public class ImageEncryptGUI extends JFrame {
 	JTextField filePath = new JTextField("filePath");
 	JButton OK = new JButton("OK");
 	JPanel South = new JPanel();
+	JButton openImage = new JButton("Open a picture.");
   
-  public static void main(String... aArgs) {
-	  ImageEncryptGUI sonya = new ImageEncryptGUI();
-	  sonya.setVisible(true);
-	  
-  }
-  
-  public ImageEncryptGUI(){
-	  this.setSize(400,500);
-	  this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-	  this.setLayout(new BorderLayout());
-	  this.setVisible(true);
-	  
-	  this.add(save,BorderLayout.NORTH);
-	  this.add(South,BorderLayout.SOUTH);
-	  South.setLayout(new GridLayout(4,2));
-	  South.add(encrypt);
-	  South.add(decrypt);
-	  
-  }
+	public static void main(String... aArgs) {
+		ImageEncryptGUI sonya = new ImageEncryptGUI();
+		sonya.setVisible(true);
+		  
+		  
+	}
+	
+	
+	public ImageEncryptGUI(){
+		this.setSize(400,500);
+		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		this.setLayout(new BorderLayout());
+		this.setVisible(true);
+		
+		this.add(save,BorderLayout.NORTH);
+		this.add(South,BorderLayout.SOUTH);
+		South.setLayout(new GridLayout(4,2));
+		South.add(encrypt);
+		South.add(decrypt);
+		encrypt.addActionListener(new ActionListener(){
+			@Override
+			public void actionPerformed(ActionEvent e) {
+					
+			}
+	
+		});
+	  }
   
 }
